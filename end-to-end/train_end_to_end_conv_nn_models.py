759b99521dbe02c28308ff9a216732258b688cb6
Denis Turcu <denis.turcu.11@gmail.com>
2024-06-15 16:44:34 -0400

    setup for training on full dataset

--- end-to-end/train_end_to_end_conv_nn_models.py
+++ end-to-end/train_end_to_end_conv_nn_models.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import h5py
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
 import lightning as L
 from collections import OrderedDict
 from electric_images_dataset import ElectricImagesDataset
-from EndToEndConvNN import EndToEndConvNN
 from EndToEndConvNN_PL import EndToEndConvNN_PL
 
 import sys
@@ -22,12 +20,13 @@ h5py_file = h5py.File(f"{data_dir_name}/responses.hdf5", "r")["responses"]
 
 dset = ElectricImagesDataset(data_dir_name=data_dir_name, fish_t=20, fish_u=30)
 
+input_noise_std = 0.25
 activation = "relu"
 layers_properties = OrderedDict(
     [
         (
             "conv1",
-            dict(in_channels=2, out_channels=8, kernel_size=5, stride=1, max_pool=dict(kernel_size=3, stride=1)),
+            dict(in_channels=2, out_channels=4, kernel_size=7, stride=1, max_pool=dict(kernel_size=3, stride=1)),
         ),
         # (
         #     "conv2",
@@ -36,9 +35,9 @@ layers_properties = OrderedDict(
         (
             "conv2",
             dict(
-                in_channels=8,
+                in_channels=4,
                 out_channels=16,
-                kernel_size=3,
+                kernel_size=5,
                 stride=1,
             ),
         ),
@@ -53,32 +52,27 @@ layers_properties = OrderedDict(
         ),
         (
             "conv4",
-            dict(in_channels=8, out_channels=2, kernel_size=3, stride=1, max_pool=dict(kernel_size=3, stride=1)),
+            dict(in_channels=8, out_channels=4, kernel_size=3, stride=1, max_pool=dict(kernel_size=3, stride=1)),
         ),
         # the fully connected layers can have dropout or flatten layers - some can miss the activation
-        ("fc1", dict(dropout=0.5, flatten=True, in_features=None, out_features=320)),
-        ("fc2", dict(dropout=0.5, in_features=320, out_features=80)),
-        ("fc3", dict(in_features=80, out_features=6, activation=False)),
+        ("fc1", dict(dropout=0.5, flatten=True, in_features=None, out_features=240)),
+        ("fc2", dict(dropout=0.5, in_features=240, out_features=60)),
+        ("fc3", dict(in_features=60, out_features=6, activation=False)),
     ]
 )
-if activation.lower() == "relu":
-    activation = nn.ReLU()
-elif activation.lower() == "tanh":
-    activation = nn.Tanh()
-else:
-    raise ValueError(f"Activation {activation} not yet supported.")
-model = EndToEndConvNN(layers_properties=layers_properties, activation=activation)  # type: ignore
+
+model_PL = EndToEndConvNN_PL(
+    layers_properties=layers_properties, activation=activation, input_noise_std=input_noise_std
+)
 
 # dummy forward pass to initialize the model
 dloader = DataLoader(dset, batch_size=4, shuffle=True)
 batch = next(iter(dloader))
-_ = model(batch[0])
+_ = model_PL.model(batch[0])
 
-train_dset, valid_dset = torch.utils.data.random_split(dset, [0.5, 0.5])  # type: ignore
-train_loader = DataLoader(train_dset, batch_size=20000, shuffle=True, drop_last=True, num_workers=12)
-valid_loader = DataLoader(valid_dset, batch_size=20000, shuffle=False, drop_last=True, num_workers=12)
-
-model_PL = EndToEndConvNN_PL(model)
+train_dset, valid_dset, _ = torch.utils.data.random_split(dset, [0.05, 0.01, 0.94])  # type: ignore
+train_loader = DataLoader(train_dset, batch_size=25_000, shuffle=True, drop_last=True, num_workers=12)
+valid_loader = DataLoader(valid_dset, batch_size=25_000, shuffle=False, drop_last=True, num_workers=12)
 
 trainer = L.Trainer(max_epochs=100, devices=[3])
 trainer.fit(model=model_PL, train_dataloaders=train_loader, val_dataloaders=valid_loader)
