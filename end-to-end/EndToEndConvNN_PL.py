af4451db3d60d2f00d4d032a29bae6e6fc8ef36c
Denis Turcu <denis.turcu.11@gmail.com>
2024-06-14 21:13:16 -0400

    adjust training hyperparams

--- end-to-end/EndToEndConvNN_PL.py
+++ end-to-end/EndToEndConvNN_PL.py
@@ -18,42 +18,41 @@ class EndToEndConvNN_PL(L.LightningModule):
         loss = nn.functional.mse_loss(y_hat, y)
         self.log("train_loss", loss)
 
-        # setup for logging non-scalars, such as figures
-        tensorboard = self.logger.experiment  # type: ignore
-
-        # log the first layer conv filters
-        filters = self.model.sequence.conv1.conv.weight.detach().cpu().numpy()
-        vval = np.max(np.abs(filters))
-        fig, ax = plt.subplots(
-            filters.shape[1], filters.shape[0], figsize=(1.5 * filters.shape[0], 1.5 * filters.shape[1])
-        )
-        for i in range(filters.shape[1]):
-            for j in range(filters.shape[0]):
-                ax[i, j].imshow(filters[j, i], cmap="seismic", vmin=-vval, vmax=vval)
-                ax[i, j].set_xticks([])
-                ax[i, j].set_yticks([])
-                sns.despine(ax=ax[i, j], left=True, bottom=True)
-        plt.tight_layout()
-        tensorboard.add_figure("filters", fig, global_step=self.global_step, close=True)
-
-        # log the training predictions as figure
-        fig = make_true_vs_predicted_figure(y.detach().cpu().numpy(), y_hat.detach().cpu().numpy())
-        tensorboard.add_figure("train_predictions", fig, global_step=self.global_step, close=True)
-
+        # # setup for logging non-scalars, such as figures
+        # tensorboard = self.logger.experiment  # type: ignore
+        # # log the first layer conv filters
+        # filters = self.model.sequence.conv1.conv.weight.detach().cpu().numpy()
+        # vval = np.max(np.abs(filters))
+        # fig, ax = plt.subplots(
+        #     filters.shape[1], filters.shape[0], figsize=(1.5 * filters.shape[0], 1.5 * filters.shape[1])
+        # )
+        # for i in range(filters.shape[1]):
+        #     for j in range(filters.shape[0]):
+        #         ax[i, j].imshow(filters[j, i], cmap="seismic", vmin=-vval, vmax=vval)
+        #         ax[i, j].set_xticks([])
+        #         ax[i, j].set_yticks([])
+        #         sns.despine(ax=ax[i, j], left=True, bottom=True)
+        # plt.tight_layout()
+        # tensorboard.add_figure("filters", fig, global_step=0, close=True)
+        # # log the training predictions as figure
+        # fig = make_true_vs_predicted_figure(y.detach().cpu().numpy()[:200], y_hat.detach().cpu().numpy()[:200])
+        # tensorboard.add_figure("train_predictions", fig, global_step=0, close=True)
+        # plt.close("all")
         return loss
 
     def validation_step(self, batch):
         x, y = batch
         y_hat = self.model(x)
         val_loss = nn.functional.mse_loss(y_hat, y)
-        self.log("val_loss", val_loss, on_epoch=True)
+        self.log("val_loss", val_loss)
 
-        # setup for logging non-scalars, such as figures
-        tensorboard = self.logger.experiment
-
-        # log the training predictions
-        fig = make_true_vs_predicted_figure(y.detach().cpu().numpy(), y_hat.detach().cpu().numpy())
-        tensorboard.add_figure("valid_predictions", fig, global_step=self.global_step, close=True)
+        # # setup for logging non-scalars, such as figures
+        # tensorboard = self.logger.experiment  # type: ignore
+        # # log the training predictions
+        # idx = np.random.permutation(y.shape[0])[:200]
+        # fig = make_true_vs_predicted_figure(y.detach().cpu().numpy()[idx], y_hat.detach().cpu().numpy()[idx])
+        # tensorboard.add_figure("valid_predictions", fig, global_step=0, close=True)
+        # plt.close("all")
         pass
 
     def configure_optimizers(self):
--- end-to-end/train_end_to_end_conv_nn_models.py
+++ end-to-end/train_end_to_end_conv_nn_models.py
@@ -29,36 +29,36 @@ layers_properties = OrderedDict(
             "conv1",
             dict(in_channels=2, out_channels=8, kernel_size=5, stride=1, max_pool=dict(kernel_size=3, stride=1)),
         ),
+        # (
+        #     "conv2",
+        #     dict(in_channels=4, out_channels=4, kernel_size=5, stride=1, max_pool=dict(kernel_size=3, stride=1)),
+        # ),
         (
             "conv2",
-            dict(in_channels=8, out_channels=32, kernel_size=5, stride=1, max_pool=dict(kernel_size=3, stride=1)),
+            dict(
+                in_channels=8,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+            ),
         ),
         (
             "conv3",
             dict(
-                in_channels=32,
-                out_channels=64,
+                in_channels=16,
+                out_channels=8,
                 kernel_size=3,
                 stride=1,
             ),
         ),
         (
             "conv4",
-            dict(
-                in_channels=64,
-                out_channels=64,
-                kernel_size=3,
-                stride=1,
-            ),
-        ),
-        (
-            "conv5",
-            dict(in_channels=64, out_channels=32, kernel_size=3, stride=1, max_pool=dict(kernel_size=3, stride=2)),
+            dict(in_channels=8, out_channels=2, kernel_size=3, stride=1, max_pool=dict(kernel_size=3, stride=1)),
         ),
         # the fully connected layers can have dropout or flatten layers - some can miss the activation
-        ("fc1", dict(dropout=0.5, flatten=True, in_features=None, out_features=512)),
-        ("fc2", dict(dropout=0.5, in_features=512, out_features=512)),
-        ("fc3", dict(in_features=512, out_features=6, activation=False)),
+        ("fc1", dict(dropout=0.5, flatten=True, in_features=None, out_features=320)),
+        ("fc2", dict(dropout=0.5, in_features=320, out_features=80)),
+        ("fc3", dict(in_features=80, out_features=6, activation=False)),
     ]
 )
 if activation.lower() == "relu":
@@ -74,11 +74,11 @@ dloader = DataLoader(dset, batch_size=4, shuffle=True)
 batch = next(iter(dloader))
 _ = model(batch[0])
 
-train_dset, valid_dset = torch.utils.data.random_split(dset, [0.8, 0.2])  # type: ignore
-train_loader = DataLoader(train_dset, batch_size=5120, shuffle=True, drop_last=True)
-valid_loader = DataLoader(valid_dset, batch_size=5120, shuffle=False, drop_last=True)
+train_dset, valid_dset = torch.utils.data.random_split(dset, [0.5, 0.5])  # type: ignore
+train_loader = DataLoader(train_dset, batch_size=20000, shuffle=True, drop_last=True, num_workers=12)
+valid_loader = DataLoader(valid_dset, batch_size=20000, shuffle=False, drop_last=True, num_workers=12)
 
 model_PL = EndToEndConvNN_PL(model)
 
-trainer = L.Trainer(max_steps=1000, devices=[3])
+trainer = L.Trainer(max_epochs=100, devices=[3])
 trainer.fit(model=model_PL, train_dataloaders=train_loader, val_dataloaders=valid_loader)
